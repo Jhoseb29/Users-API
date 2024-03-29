93bfeadc3146a553e3687b8c0501707e2a3290a5
Juan_lamouroux <juan.lamouroux@jala.university>
2024-03-29 12:14:57 -0500

    application correction

--- src/test/java/university/jala/usersapi/UsersApiApplicationTests.java
+++ src/test/java/university/jala/usersapi/UsersApiApplicationTests.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = UsersApiApplication.class)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
 class UsersApiApplicationTests {
 
   @Test
