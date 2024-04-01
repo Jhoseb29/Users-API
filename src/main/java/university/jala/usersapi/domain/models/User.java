a252f23b7aa4974e208d57e202012cf74aa34ff8
Fernando Pinto Villarroel <Fernando.Pinto@jala.university>
2024-04-01 12:16:47 -0400

    US-0104/2: Add User Validation with TDD

--- src/main/java/university/jala/usersapi/domain/models/User.java
+++ src/main/java/university/jala/usersapi/domain/models/User.java
@@ -2,15 +2,19 @@ package university.jala.usersapi.domain.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -27,27 +31,59 @@ import org.springframework.security.core.userdetails.UserDetails;
 public final class User implements UserDetails {
 
   /**
+   * ID Max Length.
+   */
+  public static final int MAX_ID_LENGTH_VALUE = 36;
+
+  /**
+   * Name Max Length.
+   */
+  public static final int MAX_NAME_LENGTH_VALUE = 200;
+
+  /**
+   * Login Max Length.
+   */
+  public static final int MAX_LOGIN_LENGTH_VALUE = 20;
+
+  /**
+   * Password Max Length.
+   */
+  public static final int MAX_PASSWORD_LENGTH_VALUE = 100;
+
+  /**
    * The unique ID of the user.
    */
   @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(
+      name = "UUID",
+      type = org.hibernate.id.uuid.UuidGenerator.class
+  )
+  @Column(length = MAX_ID_LENGTH_VALUE)
   private String id;
 
   /**
    * The name of the user.
    */
-  @Column
+  @Column(nullable = false, length = MAX_NAME_LENGTH_VALUE)
+  @NotBlank(message = "The field 'name' cannot be empty.")
+  @NotNull(message = "The field 'name' cannot be null.")
   private String name;
 
   /**
    * The login of the user.
    */
-  @Column(nullable = false)
+  @Column(nullable = false, length = MAX_LOGIN_LENGTH_VALUE)
+  @NotBlank(message = "The field 'login' cannot be empty.")
+  @NotNull(message = "The field 'login' cannot be null.")
   private String login;
 
   /**
    * The password of the user.
    */
-  @Column
+  @Column(nullable = false, length = MAX_PASSWORD_LENGTH_VALUE)
+  @NotBlank(message = "The field 'password' cannot be empty.")
+  @NotNull(message = "The field 'password' cannot be null.")
   private String password;
 
   @Override
