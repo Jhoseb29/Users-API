78a1f93f3916e385104bf83f9ed39cf3a1801192
Fernando Pinto Villarroel <Fernando.Pinto@jala.university>
2024-04-02 12:59:54 -0400

    Fix/US-0108/2: Fix checkstyle

--- src/main/java/university/jala/usersapi/domain/service/security/configuration/CustomAuthenticationEntryPoint.java
+++ src/main/java/university/jala/usersapi/domain/service/security/configuration/CustomAuthenticationEntryPoint.java
@@ -7,12 +7,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+/**
+ * Custom authentication entry point to handle unauthorized access.
+ */
+public final class CustomAuthenticationEntryPoint
+    implements AuthenticationEntryPoint {
 
+  /**
+   * Invoked when an unauthenticated user tries to access a secured resource.
+   *
+   * @param request       the request being handled
+   * @param response      the response to be sent back to the client
+   * @param authException the exception that caused the unauthorized access
+   * @throws IOException if an I/O error occurs while writing the response
+   */
   @Override
-  public void commence(HttpServletRequest request,
-      HttpServletResponse response,
-      AuthenticationException authException) throws IOException, java.io.IOException {
+  public void commence(final HttpServletRequest request,
+      final HttpServletResponse response,
+      final AuthenticationException authException)
+      throws IOException, java.io.IOException {
 
     String message
         = "You're not authenticated. "
