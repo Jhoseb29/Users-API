10d0325b1bdef4b85c0d0688e55e95a065248b4f
Juan_lamouroux <juan.lamouroux@jala.university>
2024-04-03 02:49:08 -0500

    Prueba v3

--- src/test/java/university/jala/usersapi/AuthenticationRequestDTOTest.java
+++ src/test/java/university/jala/usersapi/AuthenticationRequestDTOTest.java
@@ -4,7 +4,7 @@ import university.jala.usersapi.domain.models.dto.AuthenticationRequestDTO;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AuthenticationRequestDTOTest {
@@ -22,8 +22,8 @@ public class AuthenticationRequestDTOTest {
     @Test
     public void test_verify_correct_login_and_password_values() {
         AuthenticationRequestDTO requestDTO = new AuthenticationRequestDTO("username", "password");
-        assertEquals("username", requestDTO.getLogin());
-        assertEquals("password", requestDTO.getPassword());
+        assertTrue(requestDTO.getLogin().equals("username"));
+        assertTrue(requestDTO.getPassword().equals("password"));
     }
 
     // Create an instance of AuthenticationRequestDTO with empty login and valid password.
@@ -40,6 +40,36 @@ public class AuthenticationRequestDTOTest {
         AuthenticationRequestDTO requestDTO = new AuthenticationRequestDTO("username", "");
         assertEquals("username", requestDTO.getLogin());
         assertEquals("", requestDTO.getPassword());
+    }
+
+    @Test
+    public void test_create_instance_with_null_login_and_valid_password() {
+        AuthenticationRequestDTO requestDTO = AuthenticationRequestDTO.builder()
+                .login(null)
+                .password("password")
+                .build();
+        assertNull(requestDTO.getLogin());
+        assertEquals("password", requestDTO.getPassword().toString());
+    }
+
+    @Test
+    public void testGetPasswordValueAfterInitialization() {
+        AuthenticationRequestDTO requestDTO = AuthenticationRequestDTO.builder()
+                .login("username")
+                .password("password")
+                .build();
+        System.out.println(requestDTO.toString());
+        assertEquals("password", requestDTO.getPassword());
+    }
+
+    @Test
+    public void testCorrectStructureRequestDTO() {
+        AuthenticationRequestDTO requestDTO = AuthenticationRequestDTO.builder()
+                .login("username")
+                .password("password")
+                .build();
+        System.out.println(requestDTO.toString());
+        assertEquals("AuthenticationRequestDTO(login=username, password=password)", requestDTO.toString());
     }
 
 }
