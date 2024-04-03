faf1a2025b9512da8f1e992c3089eef22f0b4d12
Lance Yapuchura <lance.yapuchura@jala.university>
2024-04-03 12:42:16 -0400

    fix test webSecurity

--- src/test/java/university/jala/usersapi/WebSecurityConfigTest.java
+++ src/test/java/university/jala/usersapi/WebSecurityConfigTest.java
@@ -8,10 +8,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import university.jala.usersapi.domain.service.security.configuration.WebSecurityConfig;
@@ -23,14 +21,17 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class WebSecurityConfigTest {
 
+    @InjectMocks
+    private WebSecurityConfig webSecurityConfig;
+
     @Mock
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Mock
     private AuthenticationProvider authenticationProvider;
 
-    @InjectMocks
-    private WebSecurityConfig webSecurityConfig;
+    @Mock
+    private HttpSecurity httpSecurity;
 
     @Before
     public void setUp() {
@@ -38,16 +39,18 @@ public class WebSecurityConfigTest {
     }
 
     @Test
-    public void testSecurityFilterChainConfiguration() throws Exception {
-        HttpSecurity httpSecurity = mock(HttpSecurity.class);
+    public void testSecurityFilterChain() throws Exception {
+        SecurityFilterChain securityFilterChain = mock(SecurityFilterChain.class);
+        when(httpSecurity.build()).thenReturn((DefaultSecurityFilterChain) securityFilterChain);
 
-        SecurityFilterChain securityFilterChain = webSecurityConfig.securityFilterChain(httpSecurity);
+        webSecurityConfig.securityFilterChain(httpSecurity);
 
-        verify(httpSecurity, times(1)).csrf((Customizer<CsrfConfigurer<HttpSecurity>>) any(AbstractHttpConfigurer.class));
+        verify(httpSecurity, times(1)).csrf(any());
         verify(httpSecurity, times(1)).authorizeHttpRequests(any());
         verify(httpSecurity, times(1)).sessionManagement(any());
         verify(httpSecurity, times(1)).authenticationProvider(authenticationProvider);
         verify(httpSecurity, times(1)).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         verify(httpSecurity, times(1)).exceptionHandling(any());
+        verify(httpSecurity, times(1)).build();
     }
 }
