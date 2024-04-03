9289aa5ac05c41286877cf1b61026f7ff64c5c65
Lance Yapuchura <lance.yapuchura@jala.university>
2024-04-03 15:23:52 -0400

    fix test of User 7

--- src/main/java/university/jala/usersapi/domain/models/User.java
+++ src/main/java/university/jala/usersapi/domain/models/User.java
@@ -8,8 +8,10 @@ import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.Collection;
 import java.util.Collections;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,90 +29,96 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor
 @Entity
 @Table(name = "users", uniqueConstraints
-    = {@UniqueConstraint(columnNames = {"login"})})
+        = {@UniqueConstraint(columnNames = {"login"})})
 public final class User implements UserDetails {
 
-  /**
-   * ID Max Length.
-   */
-  public static final int MAX_ID_LENGTH_VALUE = 36;
+    /**
+     * ID Max Length.
+     */
+    public static final int MAX_ID_LENGTH_VALUE = 36;
 
-  /**
-   * Name Max Length.
-   */
-  public static final int MAX_NAME_LENGTH_VALUE = 200;
+    /**
+     * Name Max Length.
+     */
+    public static final int MAX_NAME_LENGTH_VALUE = 200;
 
-  /**
-   * Login Max Length.
-   */
-  public static final int MAX_LOGIN_LENGTH_VALUE = 20;
+    /**
+     * Login Max Length.
+     */
+    public static final int MAX_LOGIN_LENGTH_VALUE = 20;
 
-  /**
-   * Password Max Length.
-   */
-  public static final int MAX_PASSWORD_LENGTH_VALUE = 100;
+    /**
+     * Password Max Length.
+     */
+    public static final int MAX_PASSWORD_LENGTH_VALUE = 100;
 
-  /**
-   * The unique ID of the user.
-   */
-  @Id
-  @GeneratedValue(generator = "uuid")
-  @GenericGenerator(name = "uuid", strategy = "uuid2")
-  @Column(length = MAX_ID_LENGTH_VALUE)
-  private String id;
+    /**
+     * The unique ID of the user.
+     */
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(length = MAX_ID_LENGTH_VALUE)
+    private String id;
 
-  /**
-   * The name of the user.
-   */
-  @Column(nullable = false, length = MAX_NAME_LENGTH_VALUE)
-  @NotBlank(message = "The field 'name' cannot be empty.")
-  @NotNull(message = "The field 'name' cannot be null.")
-  private String name;
+    /**
+     * The name of the user.
+     */
+    @Column(nullable = false, length = MAX_NAME_LENGTH_VALUE)
+    @NotBlank(message = "The field 'name' cannot be empty.")
+    @NotNull(message = "The field 'name' cannot be null.")
+    private String name;
 
-  /**
-   * The login of the user.
-   */
-  @Column(nullable = false, length = MAX_LOGIN_LENGTH_VALUE)
-  @NotBlank(message = "The field 'login' cannot be empty.")
-  @NotNull(message = "The field 'login' cannot be null.")
-  private String login;
+    /**
+     * The login of the user.
+     */
+    @Column(nullable = false, length = MAX_LOGIN_LENGTH_VALUE)
+    @NotBlank(message = "The field 'login' cannot be empty.")
+    @NotNull(message = "The field 'login' cannot be null.")
+    private String login;
 
-  /**
-   * The password of the user.
-   */
-  @Column(nullable = false, length = MAX_PASSWORD_LENGTH_VALUE)
-  @NotBlank(message = "The field 'password' cannot be empty.")
-  @NotNull(message = "The field 'password' cannot be null.")
-  private String password;
+    /**
+     * The password of the user.
+     */
+    @Column(nullable = false, length = MAX_PASSWORD_LENGTH_VALUE)
+    @NotBlank(message = "The field 'password' cannot be empty.")
+    @NotNull(message = "The field 'password' cannot be null.")
+    private String password;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return Collections.emptyList();
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public String getUsername() {
-    return login;
-  }
+    @Override
+    public String getUsername() {
+        return login;
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User{id='%s', name='%s', login='%s', password='%s'}",
+                id, name, login, password);
+    }
 
 }
